720b6969bda0c4ddf8b3094266a0c3ab6ca3e68a
Eric W. Greene <eric@t4d.io>
2024-04-22 21:02:03 +0000

    add dictionary demo

--- demos/hello_world/explore_dictionaries.py
+++ demos/hello_world/explore_dictionaries.py
@@ -1,16 +1,20 @@
-person = {
-    "first_name": "Bob",
-    "last_name": "Smith",
-    "age": 23,
-}
+from typing import Any
 
+# creating a dictionary object and assigning to the person variable
+person: dict[str, Any] = {"first_name": "Bob", "last_name": "Smith", "age": 23}
+
+# printing the current key-value pairs
 print(person["first_name"])
 print(person["last_name"])
 print(person["age"])
 
+# print the whole dictionary
 print(person)
 
+# update the existing key-value pair
 person["age"] = 24
+
+# add a new key-value pair
 person["hair_color"] = "brown"
 
 # first_name Bob
@@ -25,7 +29,12 @@ try:
     print(person["middle_name"])
 except KeyError:
     print("KeyError: middle_name")
+except Exception as e:
+    print("Exception: ", e)
 
 # safely access a key that may not exist, and
 # use a default value if the key is not found
 print(person.get("middle_name", "N/A"))
+
+# remove a key-value pair
+del person["hair_color"]
