ad770882072d273ad326e4c08505c081c4746327
Cory Lee <clee@esri.com>
2024-04-23 18:04:17 +0000

    exercise 4

--- exercises/execise_4.py
+++ exercises/execise_4.py
@@ -1,27 +1,35 @@
-from typing import Any
+from typing import Any, Callable
+
 
 def input_str(prompt: str) -> str:
-    return input(prompt)
+    return input(prompt + " > ")
+
 
 def input_float(prompt: str) -> float:
     return float(input_str(prompt))
 
+
 def input_int(prompt: str) -> int:
     return int(input_str(prompt))
 
+
 def get_command() -> str:
-    return input_str("Enter a command: ")
+    return input_str("Enter a command")
+
 
 def get_operand() -> float:
-    return input_float("Enter operand: ")
+    return input_float("Enter operand")
+
 
 def get_entry_id() -> int:
-    return input_int("Enter entry id: ")
+    return input_int("Enter entry id")
+
 
 def next_entry_id(history: list[dict[str, Any]]) -> int:
     if history:
         return max(entry["id"] for entry in history) + 1
     return 1
+
 
 def append_history_entry(
     operation: str, operand: float, history: list[dict[str, Any]]
@@ -34,65 +42,80 @@ def append_history_entry(
         }
     )
 
+
+def remove_history_entry(entry_id: int, history: list[dict[str, Any]]) -> None:
+    for entry in history:
+        if entry["id"] == entry_id:
+            history.remove(entry)
+            break
+
+
 def clear_history_entries(history: list[dict[str, Any]]) -> None:
     history.clear()
+
+
+def print_history_entries(history: list[dict[str, Any]]) -> None:
+    for entry in history:
+        entry_id = entry["id"]
+        entry_command = entry["command"]
+        entry_operand = entry["operand"]
+        print(
+            (
+                f"id: {entry_id}, "
+                f"command: {entry_command}, "
+                f"operand: {entry_operand}"
+            )
+        )
+
+
+def print_invalid_command(command: str) -> None:
+    print(f"Invalid command: {command}")
+
+
+def print_result(result: float) -> None:
+    print(f"Result: {result}")
+
+
+def print_error(message: str) -> None:
+    print(f"Error: {message}")
+
 
 def main() -> None:
     result = 0.0
     history: list[dict[str, Any]] = []
-    history_id = 0
+
+    calc_fns: dict[str, Callable[[float, float], float]] = {
+        "add": lambda x, y: x + y,
+        "subtract": lambda x, y: x - y,
+        "multiply": lambda x, y: x * y,
+        "divide": lambda x, y: x / y,
+    }
 
     while True:
         command = get_command()
 
-        if command == "add":
+        if command in calc_fns:
             operand = get_operand()
-            result = result + operand
-            history_id += 1
-            append_history_entry("add", operand, history)
-        elif command == "subtract":
-            operand = get_operand()
-            result = result - operand
-            history_id += 1
-            append_history_entry("subtract", operand, history)
-        elif command == "multiply":
-            operand = get_operand()
-            result = result * operand
-            history_id += 1
-            append_history_entry("multiply", operand, history)
-        elif command == "divide":
-            operand =  get_operand()
-            result = result / operand
-            history_id += 1
-            append_history_entry("divide", operand, history)
+            if command == "divide" and operand == 0:
+                print_error("Cannot divide by zero")
+                continue
+            calc_fn = calc_fns[command]
+            result = calc_fn(result, operand)
+            append_history_entry(command, operand, history)
         elif command == "history":
-            for entry in history:
-                entry_id = entry["id"]
-                entry_command = entry["command"]
-                entry_operand = entry["operand"]
-                print(
-                    (
-                        f"id: {entry_id}, "
-                        f"command: {entry_command}, "
-                        f"operand: {entry_operand}"
-                    )
-                )
+            print_history_entries(history)
         elif command == "remove":
-            entry_id = int(input("Enter entry id: "))
-            for entry in history:
-                if entry["id"] == entry_id:
-                    history.remove(entry)
-                    break
+            remove_history_entry(get_entry_id(), history)
         elif command == "clear":
             result = 0.0
             clear_history_entries(history)
         elif command == "exit":
             return
         else:
-            print(f"Invalid command: {command}")
+            print_invalid_command(command)
             continue
 
-        print(f"Result: {result}")
+        print_result(result)
 
 
 if __name__ == "__main__":
