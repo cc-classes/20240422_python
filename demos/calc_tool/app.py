e10b85aa79a1ed34fee0f466ebac850402e987dc
Eric W. Greene <eric@t4d.io>
2024-04-25 18:36:43 +0000

    add solution to exercise 11

--- demos/calc_tool/app.py
+++ demos/calc_tool/app.py
@@ -21,63 +21,100 @@ def create_history_factory(kind: str = "obj") -> History:
     return HistoryObj()
 
 
+class InvalidCommandFormatError(Exception): ...
+
+
+def parse_command(command: str) -> tuple[str, str | None]:
+    import re
+
+    command_without_args_pattern = re.compile(r"^[a-z]+$")
+    command_with_args_pattern = re.compile(r"^([a-z]+) (\S+)$")
+
+    command_with_args_match = command_with_args_pattern.match(command)
+
+    if command_with_args_match:
+        return command_with_args_match.groups()
+    elif command_without_args_pattern.match(command):
+        return (command, None)
+    else:
+        raise InvalidCommandFormatError(command)
+
+
 class CalculatorTool:
     def __init__(self, history: History):
         self.__history = history
 
         self.__history_storage = HistoryStorage(self.__history)
 
+    def display_result(self) -> None:
+        try:
+            print_result(calculator_result(self.__history))
+        except ZeroDivisionError as e:
+            logging.error(e)
+            print(
+                (
+                    "Unable to display result, error in history of "
+                    "operations, please clear or remove before proceeding,"
+                )
+            )
+
+    def command_calculation(self, command_name: str, command_arg: str) -> None:
+        try:
+            operand = float(command_arg)
+            if command_name == "divide" and operand == 0:
+                raise ZeroDivisionError("Cannot divide by zero")
+            self.__history.append_history_entry(command_name, operand)
+            self.display_result()
+        except ValueError as e:
+            logging.error(e)
+            print(f"Invalid math operand: {command_arg}")
+        except ZeroDivisionError as e:
+            logging.error(e)
+            print("Invalid division by zero")
+
     def run(self) -> None:
         while True:
-            command = get_command()
+            try:
+                command_name, command_arg = parse_command(get_command())
 
-            with open("command-log.txt", "a") as command_log_file:
-                command_log_file.write(
-                    (
-                        f"timestamp: {datetime.now().isoformat()}"
-                        f", command: {command}\n"
+                with open("command-log.txt", "a") as command_log_file:
+                    command_log_file.write(
+                        (
+                            f"timestamp: {datetime.now().isoformat()}"
+                            f", command: {command_name}, args: {command_arg}\n"
+                        )
                     )
-                )
 
-            if command in calc_fns:
-                try:
-                    operand = get_operand()
-                    if command == "divide" and operand == 0:
-                        raise ZeroDivisionError("Cannot divide by zero")
-                    self.__history.append_history_entry(command, operand)
-                except ZeroDivisionError as e:
-                    logging.error(e)
-                    continue
-            elif command == "history":
+            except InvalidCommandFormatError as e:
+                logging.error(e)
+                print("Invalid command format")
+                continue
+
+            if command_name in calc_fns:
+                self.command_calculation(command_name, command_arg)
+            elif command_name == "history":
                 HistoryConsoleReporter(self.__history).print_history_entries()
                 HistoryFileReporter(
                     self.__history, "entries.txt"
                 ).print_history_entries()
-            elif command == "remove":
-                self.__history.remove_history_entry(get_entry_id())
-            elif command == "save":
-                self.__history_storage.save_history()
-            elif command == "load":
-                self.__history_storage.load_history()
-            elif command == "clear":
+            elif command_name == "remove":
+                # add error handling
+                self.__history.remove_history_entry(int(command_arg))
+            elif command_name == "save":
+                if not command_arg:
+                    command_arg = "history.json"
+                self.__history_storage.save_history(command_arg)
+            elif command_name == "load":
+                if not command_arg:
+                    command_arg = "history.json"
+                self.__history_storage.load_history(command_arg)
+            elif command_name == "clear":
                 self.__history.clear_history_entries()
-            elif command == "exit":
+            elif command_name == "exit":
                 return
             else:
-                print_invalid_command(command)
-                logging.warning(f"Invalid command: {command}")
-                continue
-
-            try:
-                print_result(calculator_result(self.__history))
-            except ZeroDivisionError as e:
-                logging.error(e)
-                print(
-                    (
-                        "Unable to display result, error in history of "
-                        "operations, please clear or remove before proceeding,"
-                    )
-                )
+                print_invalid_command(command_name)
+                logging.warning(f"Invalid command: {command_name}")
 
 
 def main() -> None:
--- demos/calc_tool/history_storage.py
+++ demos/calc_tool/history_storage.py
@@ -7,10 +7,10 @@ class HistoryStorage:
     def __init__(self, history: History) -> None:
         self.__history = history
 
-    def save_history(self) -> None:
-        with open("history.json", "w") as file:
+    def save_history(self, file_name: str) -> None:
+        with open(file_name, "w") as file:
             json.dump(list(self.__history), file)
 
-    def load_history(self) -> None:
-        with open("history.json", "r") as file:
+    def load_history(self, file_name: str) -> None:
+        with open(file_name, "r") as file:
             self.__history.bulk_import(json.load(file))
