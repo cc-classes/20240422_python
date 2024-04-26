644fa447b41ca6c120540a0405a565f857726101
Eric W. Greene <eric@t4d.io>
2024-04-26 22:33:36 +0000

    add solution to stock mini-lab

--- demos/features/explore_requests.py
+++ demos/features/explore_requests.py
@@ -1,24 +1,25 @@
 import requests
 import re
+import json
+from pathlib import Path
 
-msft_price_pattern = re.compile(r"406\.32")
+# Mini-lab
+
+# 1. Select 5 stock symbols, get the price for each one, and save the price and symbol for each one to a single JSON file.
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
-
-resp = requests.get(
-    "https://finance.yahoo.com/quote/MSFT", headers=headers
-)
-# print(resp.text)
-
-found_match = msft_price_pattern.search(resp.text)
-
-print(
-    found_match
-)  # use the match on the specific price, to access the surrounding html
-
-# then use the surrounding html to do a capture pattern to get the price no matter what it
 price_pattern = re.compile(r"active><span>(.*?)</span></fin-streamer>")
+symbols = ["MSFT", "T", "AAPL", "AMZN", "FB"]
+result = {}
 
-print(float(price_pattern.search(resp.text).groups()[0]))
+for symbol in symbols:
+    resp = requests.get(
+        f"https://finance.yahoo.com/quote/{symbol}", headers=headers
+    )
+
+    result[symbol] = float(price_pattern.search(resp.text).groups()[0])
+
+with Path("output.json").open("w", encoding="UTF-8") as output_file:
+    json.dump(result, output_file, indent=2)
